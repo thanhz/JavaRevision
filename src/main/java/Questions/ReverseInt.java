eef1be0dcd9d6f4a9b37c6ce097ad92d54033cad
Thanh <thanhz@hotmail.com>
2020-11-18 01:01:37 +0000

    Added alternative improved solution to reverseInt

--- src/main/java/Questions/ReverseInt.java
+++ src/main/java/Questions/ReverseInt.java
@@ -15,25 +15,20 @@ public class ReverseInt {
         }
 
         long reversed = Long.parseLong(stringResult.toString());
-        if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
             return 0;
         } else return Math.toIntExact(reversed);
+    }
 
-
-/*        String temps = "" + x;
-        char end = temps.charAt(2);
-        char middle = temps.charAt(1);
-        char beginning = temps.charAt(0);
-        String reversed = "" + end + middle + beginning;
-        int results = Integer.parseInt(reversed);*/
-
-/*        int thousands = x % 1000;
-        int hundreds = thousands % 100;
-        int tens = hundreds % 10;
-        System.out.println("thousands " + thousands);
-        System.out.println("hundreds " + hundreds);
-        System.out.println("tens " + tens);
-        String temp = "" + tens + hundreds;
-        int results = Integer.parseInt(temp);*/
+    public int reverse2(int input) {
+        int reversed = 0;
+        while (input != 0) {
+            reversed = reversed * 10 + input % 10;
+            input /= 10;
+            if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return reversed;
     }
 }
--- src/test/java/ReverseIntTestSpec.java
+++ src/test/java/ReverseIntTestSpec.java
@@ -20,4 +20,20 @@ public class ReverseIntTestSpec {
         int expected = -321;
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void reverseInt2ShouldReturnReversedInteger() {
+        ReverseInt tester = new ReverseInt();
+        int actual = tester.reverse2(54321);
+        int expected = 12345;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseInt2ShouldCorrectlyReverseNegativeNumbers() {
+        ReverseInt tester = new ReverseInt();
+        int actual = tester.reverse2(-123);
+        int expected = -321;
+        assertEquals(expected, actual);
+    }
 }
